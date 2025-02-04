940b1d8fb2cb1f75a8633a224a45b125bf59e5c1
Samir Diaz <samir.diaz.1206@gmail.com>
2025-02-04 17:07:12 -0500

    Release 1 de proyecto

---
+++ Domain/CustomClaim.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain;
+
+public class CustomClaim
+{
+  public required string Name { get; set; }
+  public required string Value { get; set; }
+}
---
+++ JwtHandler/Handler.cs
@@ -0,0 +1,136 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Domain;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtHandler;
+
+public static class Handler
+{
+  private static string? Issuer { get; set; }
+  private static string? Audience { get; set; }
+  private static string? Key { get; set; }
+  private static string? RefreshKey { get; set; }
+  private static string? EncryptionKey { get; set; }
+  
+  public static void Initialize(string issuer, string audience, string key, string refreshKey, string encryptionKey) {
+    Issuer = issuer;
+    Audience = audience;
+    Key = key;
+    RefreshKey = refreshKey;
+    EncryptionKey = encryptionKey;
+  }
+
+  public static TokenValidationParameters AccessValidationParameters() {
+    if (Issuer == null || Audience == null || Key == null || EncryptionKey == null) throw new ArgumentNullException("Invalid configuration parameters");
+
+    return new TokenValidationParameters
+    {
+      ValidateIssuer = true,
+      ValidateAudience = true,
+      ValidateLifetime = true,
+      ValidateIssuerSigningKey = true,
+      ValidIssuer = Issuer,
+      ValidAudience = Audience,
+      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Key!)),
+      TokenDecryptionKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(EncryptionKey!)),
+    };
+  }
+
+  public static TokenValidationParameters RefreshValidationParameters() {
+    if (Issuer == null || Audience == null || RefreshKey == null || EncryptionKey == null) throw new ArgumentNullException("Invalid configuration parameters");
+    
+    return new TokenValidationParameters
+    {
+      ValidateIssuer = true,
+      ValidateAudience = true,
+      ValidateLifetime = true,
+      ValidateIssuerSigningKey = true,
+      ValidIssuer = Issuer,
+      ValidAudience = Audience,
+      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(RefreshKey!)),
+      TokenDecryptionKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(EncryptionKey!)),
+    };
+  }
+
+  public static string GenerateAccessToken(Guid userId, Guid sessionId, string role, CustomClaim[]? customClaims = null, int time = 5) {
+    if (string.IsNullOrEmpty(Key) || Encoding.UTF8.GetBytes(Key!).Length < 32) throw new Exception("Invalid key");
+    
+    if (string.IsNullOrEmpty(EncryptionKey) || Encoding.UTF8.GetBytes(EncryptionKey!).Length != 32) throw new Exception("Invalid encryption key");
+
+    SymmetricSecurityKey key = new(Encoding.UTF8.GetBytes(Key!));
+    SigningCredentials credentials = new(key, SecurityAlgorithms.HmacSha256);
+
+    Claim[] claims =
+    [
+        new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+        new Claim(ClaimTypes.Role, role),
+        new Claim(ClaimTypes.Sid, sessionId.ToString())
+    ];
+
+    if (customClaims != null) {
+      foreach (CustomClaim claim in customClaims) {
+        claims.Append(new Claim(claim.Name, claim.Value));
+      }
+    }
+
+    ClaimsIdentity subject = new(claims);
+
+    SymmetricSecurityKey encryptionKey = new(Encoding.UTF8.GetBytes(EncryptionKey!));
+    EncryptingCredentials encryptingCredentials = new(encryptionKey, SecurityAlgorithms.Aes256KW, SecurityAlgorithms.Aes256CbcHmacSha512);
+
+    JwtSecurityToken encryptedToken = new JwtSecurityTokenHandler().CreateJwtSecurityToken(
+        issuer: Issuer,
+        audience: Audience,
+        subject: subject,
+        notBefore: DateTime.Now,
+        expires: DateTime.Now.AddMinutes(time),
+        issuedAt: DateTime.Now,
+        encryptingCredentials: encryptingCredentials,
+        signingCredentials: credentials
+    );
+
+    return new JwtSecurityTokenHandler().WriteToken(encryptedToken);
+  }
+
+  public static string GenerateRefreshToken(Guid userId, Guid sessionId, string role, CustomClaim[]? customClaims = null, int time = 15) {
+    if (string.IsNullOrEmpty(RefreshKey) || Encoding.UTF8.GetBytes(RefreshKey!).Length < 32) throw new Exception("Invalid key");
+    
+    if (string.IsNullOrEmpty(EncryptionKey) || Encoding.UTF8.GetBytes(EncryptionKey!).Length != 32) throw new Exception("Invalid encryption key");
+
+    SymmetricSecurityKey key = new(Encoding.UTF8.GetBytes(RefreshKey!));
+    SigningCredentials credentials = new(key, SecurityAlgorithms.HmacSha256);
+
+    Claim[] claims =
+    [
+        new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+        new Claim(ClaimTypes.Role, role),
+        new Claim(ClaimTypes.Sid, sessionId.ToString())
+    ];
+
+    if (customClaims != null) {
+      foreach (CustomClaim claim in customClaims) {
+        claims.Append(new Claim(claim.Name, claim.Value));
+      }
+    }
+
+    ClaimsIdentity subject = new(claims);
+
+    SymmetricSecurityKey encryptionKey = new(Encoding.UTF8.GetBytes(EncryptionKey!));
+    EncryptingCredentials encryptingCredentials = new(encryptionKey, SecurityAlgorithms.Aes256KW, SecurityAlgorithms.Aes256CbcHmacSha512);
+
+    JwtSecurityToken encryptedToken = new JwtSecurityTokenHandler().CreateJwtSecurityToken(
+        issuer: Issuer,
+        audience: Audience,
+        subject: subject,
+        notBefore: DateTime.Now,
+        expires: DateTime.Now.AddMinutes(time),
+        issuedAt: DateTime.Now,
+        encryptingCredentials: encryptingCredentials,
+        signingCredentials: credentials
+    );
+
+    return new JwtSecurityTokenHandler().WriteToken(encryptedToken);
+  }
+}
